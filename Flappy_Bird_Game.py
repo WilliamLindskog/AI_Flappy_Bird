802b11c6e49bba5bb2e2554b52513bf75aa00065
WilliamLindskog <williamlindskog@outlook.com>
2020-11-20 14:53:31 +0100

    Move function added

--- Flappy_Bird_Game.py
+++ Flappy_Bird_Game.py
@@ -35,6 +35,39 @@ class Bird:
         self.height = self.y
 
     def move(self):
+        self.tick_count += 1
+
+        d = self.vel*self.tick_count + 1.5*self.tick_count**2
+
+        if d >= 16:
+            d = 16
+
+        if d < 0:
+            d -= 2
+
+        self.y = self.y + d
+
+        if d < 0 or self.y < self.height + 50:
+            if self.tilt < self.Max_Rotation:
+                self.tilt = self.Max_Rotation
+        else:
+            if self.tilt > -90:
+                self.tilt -= self.Rot_Vel
+
+    def draw(self, win):
+        self.img_count += 1
+
+        if self.img_count < self.Animation_Time:
+            self.img = self.Images[0]
+        elif self.img_count < self.Animation_Time*2:
+            self.img = self.Images[1]
+        elif self.img_count < self.Animation_Time*3:
+            self.img = self.Images[2]
+        elif self.img_count < self.Animation_Time*4:
+            self.img = self.Images[1]
+        elif self.img_count == self.Animation_Time*4 + 1:
+            self.img = self.Images[0]
+            self.img_count = 0
 
 while True:
     bird.move()
